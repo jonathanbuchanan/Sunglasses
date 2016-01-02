5fb25b078a1ea90955d731a04f337799a5f058f6
Jonathan Buchanan <jonathan.russ.buchanan@gmail.com>
2016-01-02 00:02:04 -0500

    Added a source directory and made a main.cpp, then setup CMake.
    Former-commit-id: a5bc6ea7a8bcafc4fba5c8f7e779d4bc3f3abad6

---
+++ Sample Projects/Feature/CMakeFiles/Feature.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Feature.dir/Source/main.cpp.o"
+  "Feature.pdb"
+  "Feature"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Feature.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
