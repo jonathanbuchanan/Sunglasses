e467f5a32025a9cdb0e870a957e7fc9d3a366df3
Jonathan Buchanan <jonathan.russ.buchanan@gmail.com>
2016-02-09 23:03:26 -0500

    Created two functions that return the bash code to set colors
    Former-commit-id: 49a4027403689a44e3752e7ef03557f99996cf3e

--- Engine/Runtime/Output/SunLogger.cpp
+++ Engine/Runtime/Output/SunLogger.cpp
@@ -2,3 +2,53 @@
 // This file is part of Sunglasses, which is licensed under the MIT License.
 // See LICENSE.md for details.
 #include "SunLogger.h"
+#include <iostream>
+
+std::map<BashColor, std::string> ColorMap = {
+    {BashColorReset,             BASH_COLOR_RESET},
+    {BashColorBright,            BASH_COLOR_BRIGHT},
+    {BashColorDim,               BASH_COLOR_DIM},
+    {BashColorUnderscore,        BASH_COLOR_UNDERSCORE},
+    {BashColorBlink,             BASH_COLOR_BLINK},
+    {BashColorReverse,           BASH_COLOR_REVERSE},
+    {BashColorHidden,            BASH_COLOR_HIDDEN},
+    
+    {BashColorForegroundBlack,   BASH_COLOR_FOREGROUND_BLACK},
+    {BashColorForegroundRed,     BASH_COLOR_FOREGROUND_RED},
+    {BashColorForegroundGreen,   BASH_COLOR_FOREGROUND_GREEN},
+    {BashColorForegroundYellow,  BASH_COLOR_FOREGROUND_YELLOW},
+    {BashColorForegroundBlue,    BASH_COLOR_FOREGROUND_BLUE},
+    {BashColorForegroundMagenta, BASH_COLOR_FOREGROUND_MAGENTA},
+    {BashColorForegroundCyan,    BASH_COLOR_FOREGROUND_CYAN},
+    {BashColorForegroundWhite,   BASH_COLOR_FOREGROUND_WHITE},
+    
+    {BashColorBackgroundBlack,   BASH_COLOR_BACKGROUND_BLACK},
+    {BashColorBackgroundRed,     BASH_COLOR_BACKGROUND_RED},
+    {BashColorBackgroundGreen,   BASH_COLOR_BACKGROUND_GREEN},
+    {BashColorBackgroundYellow,  BASH_COLOR_BACKGROUND_YELLOW},
+    {BashColorBackgroundBlue,    BASH_COLOR_BACKGROUND_BLUE},
+    {BashColorBackgroundMagenta, BASH_COLOR_BACKGROUND_MAGENTA},
+    {BashColorBackgroundCyan,    BASH_COLOR_BACKGROUND_CYAN},
+    {BashColorBackgroundWhite,   BASH_COLOR_BACKGROUND_WHITE}
+};
+
+string getBashColorSetter(std::vector<BashColor> colors) {
+    string colorString = BASH_COLOR_PREFIX;
+    
+    for (BashColor color : colors)
+        colorString += ColorMap[color];
+    
+    colorString += BASH_COLOR_SUFFIX;
+    
+    return colorString;
+}
+
+string getBashColorSetter(BashColor color) {
+    string colorString = BASH_COLOR_PREFIX;
+    
+    colorString += ColorMap[color];
+    
+    colorString += BASH_COLOR_SUFFIX;
+    
+    return colorString;
+}
--- Engine/Runtime/Output/SunLogger.h
+++ Engine/Runtime/Output/SunLogger.h
@@ -5,39 +5,74 @@
 #define SUNLOGGER_H
 
 #include <string>
+#include <vector>
+#include <map>
 
 #include "SunNode.h"
 #include "SunService.h"
 
-const std::string BASH_COLOR_PREFIX          = "\033[";
-const std::string BASH_COLOR_SEPARATOR           = ",";
-const std::string BASH_COLOR_SUFFIX              = "m";
+const std::string BASH_COLOR_PREFIX             = "\033[";
+const std::string BASH_COLOR_SEPARATOR          =     ";";
+const std::string BASH_COLOR_SUFFIX             =     "m";
 
-const std::string BASH_COLOR_RESET               = "0";
-const std::string BASH_COLOR_BRIGHT              = "1";
-const std::string BASH_COLOR_DIM                 = "2";
-const std::string BASH_COLOR_UNDERSCORE          = "4";
-const std::string BASH_COLOR_BLINK               = "5";
-const std::string BASH_COLOR_REVERSE             = "7";
-const std::string BASH_COLOR_HIDDEN              = "8";
+const std::string BASH_COLOR_RESET              =     "0";
+const std::string BASH_COLOR_BRIGHT             =     "1";
+const std::string BASH_COLOR_DIM                =     "2";
+const std::string BASH_COLOR_UNDERSCORE         =     "4";
+const std::string BASH_COLOR_BLINK              =     "5";
+const std::string BASH_COLOR_REVERSE            =     "7";
+const std::string BASH_COLOR_HIDDEN             =     "8";
 
-const std::string BASH_COLOR_FOREGROUND_BLACK   = "30";
-const std::string BASH_COLOR_FOREGROUND_RED     = "31";
-const std::string BASH_COLOR_FOREGROUND_GREEN   = "32";
-const std::string BASH_COLOR_FOREGROUND_YELLOW  = "33";
-const std::string BASH_COLOR_FOREGROUND_BLUE    = "34";
-const std::string BASH_COLOR_FOREGROUND_MAGENTA = "35";
-const std::string BASH_COLOR_FOREGROUND_CYAN    = "36";
-const std::string BASH_COLOR_FOREGROUND_WHITE   = "37";
+const std::string BASH_COLOR_FOREGROUND_BLACK   =    "30";
+const std::string BASH_COLOR_FOREGROUND_RED     =    "31";
+const std::string BASH_COLOR_FOREGROUND_GREEN   =    "32";
+const std::string BASH_COLOR_FOREGROUND_YELLOW  =    "33";
+const std::string BASH_COLOR_FOREGROUND_BLUE    =    "34";
+const std::string BASH_COLOR_FOREGROUND_MAGENTA =    "35";
+const std::string BASH_COLOR_FOREGROUND_CYAN    =    "36";
+const std::string BASH_COLOR_FOREGROUND_WHITE   =    "37";
 
-const std::string BASH_COLOR_BACKGROUND_BLACK   = "40";
-const std::string BASH_COLOR_BACKGROUND_RED     = "41";
-const std::string BASH_COLOR_BACKGROUND_GREEN   = "42";
-const std::string BASH_COLOR_BACKGROUND_YELLOW  = "43";
-const std::string BASH_COLOR_BACKGROUND_BLUE    = "44";
-const std::string BASH_COLOR_BACKGROUND_MAGENTA = "45";
-const std::string BASH_COLOR_BACKGROUND_CYAN    = "46";
-const std::string BASH_COLOR_BACKGROUND_WHITE   = "47";
+const std::string BASH_COLOR_BACKGROUND_BLACK   =    "40";
+const std::string BASH_COLOR_BACKGROUND_RED     =    "41";
+const std::string BASH_COLOR_BACKGROUND_GREEN   =    "42";
+const std::string BASH_COLOR_BACKGROUND_YELLOW  =    "43";
+const std::string BASH_COLOR_BACKGROUND_BLUE    =    "44";
+const std::string BASH_COLOR_BACKGROUND_MAGENTA =    "45";
+const std::string BASH_COLOR_BACKGROUND_CYAN    =    "46";
+const std::string BASH_COLOR_BACKGROUND_WHITE   =    "47";
+
+enum BashColor {
+    BashColorReset,
+    BashColorBright,
+    BashColorDim,
+    BashColorUnderscore,
+    BashColorBlink,
+    BashColorReverse,
+    BashColorHidden,
+    
+    BashColorForegroundBlack,
+    BashColorForegroundRed,
+    BashColorForegroundGreen,
+    BashColorForegroundYellow,
+    BashColorForegroundBlue,
+    BashColorForegroundMagenta,
+    BashColorForegroundCyan,
+    BashColorForegroundWhite,
+    
+    BashColorBackgroundBlack,
+    BashColorBackgroundRed,
+    BashColorBackgroundGreen,
+    BashColorBackgroundYellow,
+    BashColorBackgroundBlue,
+    BashColorBackgroundMagenta,
+    BashColorBackgroundCyan,
+    BashColorBackgroundWhite
+};
+
+extern std::map<BashColor, std::string> ColorMap;
+
+extern string getBashColorSetter(std::vector<BashColor> colors);
+extern string getBashColorSetter(BashColor color);
 
 class SunLogger : public SunService {
 public:
