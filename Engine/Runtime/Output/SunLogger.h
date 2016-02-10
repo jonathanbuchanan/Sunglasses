9c357cef10a7e9ba73ed6af95a7260f9fe6d468f
Jonathan Buchanan <jonathan.russ.buchanan@gmail.com>
2016-02-09 21:20:43 -0500

    Created the Output directory in the Runtime directory and created the SunLogger .h and .cpp files
    Former-commit-id: c9b59b30d409a3938f20e85dc547434819b8a747

---
+++ Engine/Runtime/Output/SunLogger.h
@@ -0,0 +1,17 @@
+// Copyright 2016 Jonathan Buchanan.
+// This file is part of Sunglasses, which is licensed under the MIT License.
+// See LICENSE.md for details.
+#ifndef SUNLOGGER_H
+#define SUNLOGGER_H
+
+#include "SunNode.h"
+#include "SunService.h"
+
+class SunLogger : public SunService {
+public:
+    
+private:
+    
+};
+
+#endif
