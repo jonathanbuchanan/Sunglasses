1863a08444960c7e1cc47a3e27a0352cf85170d2
Jonathan Buchanan <jonathan.russ.buchanan@gmail.com>
2016-02-09 22:00:04 -0500

    Defined constants for strings used in setting colors in bash
    Former-commit-id: e915cf1c2bf68cef27bb40b89a5215c447d94b04

--- Engine/Runtime/Output/SunLogger.h
+++ Engine/Runtime/Output/SunLogger.h
@@ -4,8 +4,40 @@
 #ifndef SUNLOGGER_H
 #define SUNLOGGER_H
 
+#include <string>
+
 #include "SunNode.h"
 #include "SunService.h"
+
+const std::string BASH_COLOR_PREFIX          = "\033[";
+const std::string BASH_COLOR_SEPARATOR           = ",";
+const std::string BASH_COLOR_SUFFIX              = "m";
+
+const std::string BASH_COLOR_RESET               = "0";
+const std::string BASH_COLOR_BRIGHT              = "1";
+const std::string BASH_COLOR_DIM                 = "2";
+const std::string BASH_COLOR_UNDERSCORE          = "4";
+const std::string BASH_COLOR_BLINK               = "5";
+const std::string BASH_COLOR_REVERSE             = "7";
+const std::string BASH_COLOR_HIDDEN              = "8";
+
+const std::string BASH_COLOR_FOREGROUND_BLACK   = "30";
+const std::string BASH_COLOR_FOREGROUND_RED     = "31";
+const std::string BASH_COLOR_FOREGROUND_GREEN   = "32";
+const std::string BASH_COLOR_FOREGROUND_YELLOW  = "33";
+const std::string BASH_COLOR_FOREGROUND_BLUE    = "34";
+const std::string BASH_COLOR_FOREGROUND_MAGENTA = "35";
+const std::string BASH_COLOR_FOREGROUND_CYAN    = "36";
+const std::string BASH_COLOR_FOREGROUND_WHITE   = "37";
+
+const std::string BASH_COLOR_BACKGROUND_BLACK   = "40";
+const std::string BASH_COLOR_BACKGROUND_RED     = "41";
+const std::string BASH_COLOR_BACKGROUND_GREEN   = "42";
+const std::string BASH_COLOR_BACKGROUND_YELLOW  = "43";
+const std::string BASH_COLOR_BACKGROUND_BLUE    = "44";
+const std::string BASH_COLOR_BACKGROUND_MAGENTA = "45";
+const std::string BASH_COLOR_BACKGROUND_CYAN    = "46";
+const std::string BASH_COLOR_BACKGROUND_WHITE   = "47";
 
 class SunLogger : public SunService {
 public:
