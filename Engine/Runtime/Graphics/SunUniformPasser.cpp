5d6aa65e83fa6ab7ce758a19c3f0ede126244eee
Jonathan Buchanan <jonathan.russ.buchanan@gmail.com>
2016-02-17 23:27:02 -0500

    Implemented UBO functionality for SunUniformPasser
    Former-commit-id: 686bceaaa2590ae8b4b13229a67c4a7881885442

--- Engine/Runtime/Graphics/SunUniformPasser.cpp
+++ Engine/Runtime/Graphics/SunUniformPasser.cpp
@@ -2,3 +2,32 @@
 // This file is part of Sunglasses, which is licensed under the MIT License.
 // See LICENSE.md for details.
 #include "SunUniformPasser.h"
+
+GLuint SunUniformPasser::bindingPoint = 0;
+
+int SunUniformPasser::createBuffer(std::string name, GLuint size) {
+	GLuint buffer;
+	glGenBuffers(1, &buffer);
+	glBindBuffer(GL_UNIFORM_BUFFER, buffer);
+	glBufferData(GL_UNIFORM_BUFFER, size, NULL, GL_STATIC_DRAW);
+
+	GLuint bindingPoint = getNewBindingPoint();
+	glBindBufferBase(GL_UNIFORM_BUFFER, bindingPoint, buffer);
+	glBindBuffer(GL_UNIFORM_BUFFER, 0);
+
+	SunUniformBuffer ubo;
+	ubo.buffer = buffer;
+	ubo.bindingPoint = bindingPoint;
+
+	addBuffer(name, ubo);
+}
+
+int SunUniformPasser::bindBufferToShader(std::string block, SunShader shader) {
+	GLuint blockIndex = glGetUniformBlockIndex(shader.getProgram(), block.c_str());
+	glUniformBlockBinding(shader.getProgram(), blockIndex, uniformBuffers[block].bindingPoint);
+}
+
+int SunUniformPasser::bufferSubData(std::string block, GLuint offset, GLuint length, const GLvoid *data) {
+	glBindBuffer(GL_UNIFORM_BUFFER, uniformBuffers[block].buffer);
+	glBufferSubData(GL_UNIFORM_BUFFER, offset, length, data);
+}
--- Engine/Runtime/Graphics/SunUniformPasser.h
+++ Engine/Runtime/Graphics/SunUniformPasser.h
@@ -5,9 +5,31 @@
 #define SUNUNIFORMPASSER_H
 
 #include "Core/SunBase.h"
+#include "Graphics/Shaders/SunShader.h" 
+#include <GL/glew.h>
+#include <glm/glm.hpp>
+#include <string>
+#include <map>
+
+struct SunUniformBuffer {
+	GLuint buffer;
+	GLuint bindingPoint;
+};
 
 class SunUniformPasser : public SunBase {
+public:
+	virtual void init();
 
+	int createBuffer(std::string name, GLuint size);
+	int bindBufferToShader(std::string block, SunShader shader);
+	int bufferSubData(std::string block, GLuint offset, GLuint length, const GLvoid *data);
+protected:
+	std::map<std::string, SunUniformBuffer> uniformBuffers;
+	inline void addBuffer(std::string s, SunUniformBuffer b) { uniformBuffers[s] = b; }
+
+	static GLuint bindingPoint;
+
+	inline GLuint getNewBindingPoint() { GLuint x = bindingPoint; bindingPoint++; return x; }
 };
 
 #endif
