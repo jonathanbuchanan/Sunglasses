10bc2d30b4278f9f390675651f0e5bead58f66d5
Jonathan Buchanan <jonathan.russ.buchanan@gmail.com>
2016-02-21 11:36:05 -0500

    Created SunWindowManager
    Former-commit-id: 4be9ce99a7cc13b00c6873cd7a848bb960239f2c

---
+++ Engine/Runtime/Graphics/SunWindowManager.h
@@ -0,0 +1,20 @@
+// Copyright 2016 Jonathan Buchanan.
+// This file is part of Sunglasses, which is licensed under the MIT License.
+// See LICENSE.md for details.
+#ifndef SUNWINDOWMANAGER_H
+#define SUNWINDOWMANAGER_H
+
+#include "Core/SunBase.h"
+#include "Core/SunService.h"
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <vector>
+
+class SunWindowManager : public SunService {
+public:
+
+private:
+
+};
+
+#endif
