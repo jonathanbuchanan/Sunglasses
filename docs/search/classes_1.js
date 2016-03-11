c738bfbf7ab15898050c4e3028015288ab4341b3
Jonathan Buchanan <jonathan.russ.buchanan@gmail.com>
2016-03-10 20:58:46 -0500

    Started using doxygen
    Former-commit-id: 1a083aabe236b054b02cdd1c9321ddb595200a83

---
+++ docs/search/classes_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['editorapp',['EditorApp',['../classEditorApp.html',1,'']]],
+  ['editorframe',['EditorFrame',['../classEditorFrame.html',1,'']]]
+];
---
+++ docs/search/searchdata.js
@@ -0,0 +1,18 @@
+var indexSectionsWithContent =
+{
+  0: "_egst",
+  1: "_egst"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes"
+};
+
