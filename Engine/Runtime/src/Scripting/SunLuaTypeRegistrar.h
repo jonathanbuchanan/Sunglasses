4f2809692ae2a544e6e810890a0a18b035fe832d
Jonathan Buchanan <jonathan.russ.buchanan@gmail.com>
2016-04-18 06:48:18 -0400

    Wrote a failing test for SunLuaTypeRegistrar

--- Engine/Runtime/src/Scripting/SunLuaTypeRegistrar.h
+++ Engine/Runtime/src/Scripting/SunLuaTypeRegistrar.h
@@ -4,5 +4,43 @@
 #ifndef SUNLUATYPEREGISTRAR_H
 #define SUNLUATYPEREGISTRAR_H
 
+#include <string>
+#include <vector>
+#include <lua.hpp>
+
+namespace SunScripting {
+    /**
+     * This struct is used as a container for a data member of class.
+     */
+    template<typename T, typename S> // T is the type of the data member, S is the class type
+    class SunLuaTypeDataMember {
+    public:
+
+    private:
+        T S::* data;
+    };
+}
+
+/**
+ * This class is used as a container for the data used in registering a type in
+ * a Lua script.
+ */
+template<typename T>
+class SunLuaTypeRegistrar {
+public:
+    /// This method registers the type within a state
+    /**
+     * @params state The lua state to register the type in
+     */
+    static void registerInState(lua_State *state) {
+    
+    }
+private:
+    /// The name of the type - this must be set for each type that is used
+    const static std::string typeName;
+
+    /// The vector of data members - this must be set for each type that is used
+    const static std::vector<void *> dataMembers;
+};
 
 #endif
---
+++ Engine/Tests/src/test_SunLuaTypeRegistrar.cpp
@@ -0,0 +1,48 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include <Sunglasses/Sunglasses.hpp>
+
+struct TestClass {
+    int x;
+    float y;
+    std::string z;
+};
+template<> const std::string SunLuaTypeRegistrar<TestClass>::typeName = "TestClass";
+
+struct SunLuaTypeRegistrarTest : ::testing::Test {
+    lua_State *L;
+
+    SunLuaTypeRegistrarTest() {
+        L = luaL_newstate();
+        luaL_openlibs(L);
+        SunLuaTypeRegistrar<TestClass>::registerInState(L);
+    }
+
+    virtual void TearDown() {
+        lua_close(L);
+    }
+};
+
+TEST_F(SunLuaTypeRegistrarTest, Constructor) {
+    luaL_dostring(L, "foo = TestClass({x = 10, y = 20.5, z = \"Hello, World!\"})");
+
+    lua_getglobal(L, "foo");
+
+    lua_pushstring(L, "x");
+    lua_gettable(L, -2);
+    EXPECT_EQ(10, (int)lua_tonumber(L, -1));
+    lua_pop(L, 1);
+
+    lua_pushstring(L, "y");
+    lua_gettable(L, -2);
+    EXPECT_DOUBLE_EQ(20.5, (double)lua_tonumber(L, -1));
+    lua_pop(L, 1);
+
+    lua_pushstring(L, "z");
+    lua_gettable(L, -2);
+    EXPECT_STREQ("Hello, World!", (const char *)lua_tostring(L, -1));
+    lua_pop(L, 1);
+
+    lua_pop(L, 1);
+}
