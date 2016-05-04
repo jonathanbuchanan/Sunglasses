0d6c9c21f9edee3da048f4d662281c5a8642e752
Jonathan Buchanan <jonathan.russ.buchanan@gmail.com>
2016-05-03 22:15:23 -0400

    Created SunServiceManager

---
+++ Engine/Runtime/src/Core/SunServiceManager.h
@@ -0,0 +1,38 @@
+// Copyright 2016 Jonathan Buchanan.
+// This file is part of Sunglasses, which is licensed under the MIT License.
+// See LICENSE.md for details.
+#ifndef SUNSERVICEMANAGER_H
+#define SUNSERVICEMANAGER_H
+#include <map>
+#include <typeindex>
+#include <memory>
+
+#include "SunService.h"
+
+/// A class that manages a map of services with types as keys
+/**
+ * This class manages a map of services with types as keys. This
+ * can be easily passed around, or shared as a static member.
+ */
+class SunServiceManager {
+public:
+    /// Accesses a service from the type given in the template parameter.
+    template<typename T>
+    std::unique_ptr<SunService> & get() {
+        return services[std::type_index(typeid(T))];
+    }
+
+    /// Assigns a service from the pointer passed.
+    /**
+     * @param service A pointer to the service to assign
+     */
+    template<typename T>
+    void assign(T *service) {
+        services[std::type_index(typeid(T))] = std::unique_ptr<SunService>(service);
+    }
+private:
+    /// The map of services
+    std::map<std::type_index, std::unique_ptr<SunService>> services;
+};
+
+#endif
