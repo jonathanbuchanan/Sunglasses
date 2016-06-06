cad850937d9d7989899809b2d305d835eaff8245
Jonathan Buchanan <jonathan.russ.buchanan@gmail.com>
2016-06-06 16:47:55 -0400

    Created the SunComponent class

---
+++ src/Core/SunComponent.h
@@ -0,0 +1,18 @@
+// Copyright 2016 Jonathan Buchanan.
+// This file is part of Sunglasses, which is licensed under the MIT License.
+// See LICENSE.md for details.
+
+/// A component of an entity
+/**
+ * A component is a part of an entity. The template parameter of this class
+ * is the type of the entity. The template parameter is needed to ensure
+ * compatibility between components and entities.
+ * @see SunEntity
+ */
+template<typename T>
+class SunComponent {
+public:
+
+private:
+
+};
