0c9b7d7f0359f80742ef37db67653444577ef29d
Jonathan Buchanan <jonathan.russ.buchanan@gmail.com>
2016-11-13 21:11:54 -0500

    Fully implemented constructors for GeometryBuffer

--- sunglasses/include/sunglasses/Graphics/GeometryBuffer.h
+++ sunglasses/include/sunglasses/Graphics/GeometryBuffer.h
@@ -3,13 +3,47 @@
 
 #include <sunglasses/Graphics/Vertex.h>
 
+#include <initializer_list>
+#include <vector>
+#include <numeric>
+
 namespace sunglasses {
 
 /// A object that contains a buffer of geometry data
 template<template<size_t, typename> class... T>
 class GeometryBuffer {
 public:
-    GeometryBuffer() {
+    /// The vertex type
+    typedef WIP::Vertex<T...> Vertex_T;
+
+    /// Constructs the buffer from a set of vertices and indices
+    GeometryBuffer(std::initializer_list<Vertex_T> _vertices, std::initializer_list<size_t> _indices) :
+            vertices(_vertices), indices(_indices) {
+        createBuffers();
+    }
+
+    /// Constructs the buffer from a set of vertices
+    GeometryBuffer(std::initializer_list<Vertex_T> _vertices) :
+            vertices(_vertices), indices(vertices.size()) {
+        std::iota(indices.begin(), indices.end(), 0);
+        createBuffers();
+    }
+
+    /// Constructs the buffer from another vector of vertices and indices
+    GeometryBuffer(const std::vector<Vertex_T> &_vertices, const std::vector<size_t> &_indices) :
+            vertices(_vertices), indices(_indices) {
+        createBuffers();
+    }
+
+    /// Constructs the buffer from another vector of vertices
+    GeometryBuffer(const std::vector<Vertex_T> &_vertices) :
+            vertices(_vertices), indices(vertices.size()) {
+        std::iota(indices.begin(), indices.end(), 0);
+        createBuffers();
+    }
+private:
+    /// Creates the OpenGL buffers
+    void createBuffers() {
         // Generate the VAO
         glGenVertexArrays(1, &VAO);
 
@@ -23,12 +57,18 @@ public:
         // Bind the VBO
         glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
+        // Fill the VBO with data
+        glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex_T), vertices.data(), GL_STATIC_DRAW);
+
 
         // Generate the EBO
         glGenBuffers(1, &EBO);
 
         // Bind the EBO
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+
+        // Fill the EBO with data
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(size_t), indices.data(), GL_STATIC_DRAW);
 
 
         // Enable vertex attributes
@@ -41,12 +81,18 @@ public:
         // Unbind the VBO
         glBindBuffer(GL_ARRAY_BUFFER, 0);
 
+        // Unbind the EBO
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
         // Unbind the VAO
         glBindVertexArray(0);
     }
-private:
-    /// The vertex type
-    typedef WIP::Vertex<T...> Vertex_T;
+
+    /// The vector containing vertex data
+    std::vector<Vertex_T> vertices;
+
+    /// The vector containing index data
+    std::vector<size_t> indices;
 
     /// The vertex array object referencing vertex data
     GLuint VAO;
